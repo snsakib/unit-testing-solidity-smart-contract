09563b7f2971f5ebaef61b509fc551b522ec91ed
snsakib <hello@snsakib.com>
2024-02-10 15:54:07 +0600

    test mintNFT function

--- test/NFTMarketplace.js
+++ test/NFTMarketplace.js
@@ -1,4 +1,8 @@
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
 describe("NFTMarketplace", function () {
   async function deploySmartContractFixture() {
@@ -11,4 +15,22 @@ describe("NFTMarketplace", function () {
 
     return { nftMarketplace, owner, user1, user2, listingPrice };
   }
+
+  describe("mintNFT", function () {
+    it("Should mint a new NFT", async () => {
+      const { nftMarketplace, user1, listingPrice } = await loadFixture(
+        deploySmartContractFixture
+      );
+
+      // Mint a new NFT
+      await nftMarketplace
+        .connect(user1)
+        .mintNFT("NFT_URI", 100000000, { value: listingPrice });
+
+      // Check if the NFT has been minted successfully
+      const allNFTs = await nftMarketplace.getAllNFTs();
+      expect(allNFTs.length).to.equal(1);
+      expect(allNFTs[0].owner).to.equal(user1.address);
+    });
+  });
 });
